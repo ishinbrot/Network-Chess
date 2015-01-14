669117506223b24b5c8637fed81fb314b35e3e04
Ian Shinbrot <ijs24@drexel.edu>
2015-01-14 18:12:38 -0500

    Added a Menu class so the user can start a new game or quit. Also included the ability to test moves

---
+++ chessticles/src/MainMenu.java
@@ -0,0 +1,110 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.event.ActionEvent;
+
+
+/**
+ * Created by ianshinbrot on 1/14/15.
+ */
+public class MainMenu extends JFrame implements ActionListener {
+    
+    public boolean blackPlayer;
+    public boolean whitePlayer;
+    public int moveResult;
+    private JMenuItem quit= new JMenuItem("Quit");
+    private JMenuItem newGame = new JMenuItem("New Game");
+    private JMenuItem testMoves = new JMenuItem("test Moves");
+    public MainMenu() {
+JFrame f = new JFrame("Start");
+        
+        JMenuBar menubar = new JMenuBar();
+        
+        JMenu menu1 = new JMenu ("Options");
+        
+        newGame.addActionListener(this);
+        quit.addActionListener(this);
+        testMoves.addActionListener(this);
+        menu1.add(newGame);
+        menu1.add(testMoves);
+        menu1.add(quit);
+        menubar.add(menu1);
+        
+        f.setJMenuBar(menubar);
+        f.setSize(300, 100);
+        f.setVisible(true);
+        System.out.println("Starting Chess Program");
+    }
+    
+    public void actionPerformed(ActionEvent e)
+    {
+        if (e.getSource() == newGame)
+        {
+            start();
+        }
+        if (e.getSource() == quit) {
+            
+            quit();
+        }
+       if (e.getSource() == testMoves) {
+           
+           testMoves();
+       }
+    }
+    public void start()
+    {
+        ChessBoard game = new ChessBoard();
+
+        game.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        game.pack();
+        game.setResizable(true);
+        game.setLocationRelativeTo(null);
+        game.setVisible(true);
+    }
+    
+    public void testMoves()
+    {
+        System.out.println("Starting Chess Program");
+
+        ChessBoard game = new ChessBoard();
+
+        game.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+        game.pack();
+        game.setResizable(true);
+        game.setLocationRelativeTo(null);
+        game.setVisible(true);
+        int startPos=12;
+        int [] start=new int[]{startPos%8,startPos/8};
+        int [] test=new int[2];
+        for (int i =0; i<64;i++){
+            int row, col;
+            row =i/8;
+            col = i%8;
+            test[0]=col;
+            test[1]=row;
+            if (MoveValidation.horizontal(start, test)||MoveValidation.vertical(start, test)||MoveValidation.diagonal(start, test)){
+                game.setSquareColor(i, Color.green);
+            }
+
+        }
+        game.setSquareColor(startPos, Color.red);
+
+    }
+    
+    
+    public void network() {
+    }
+    
+    public void load() {
+        
+        
+    }
+    
+    
+    public void quit() {
+        
+        System.exit(2);
+    }
+}
