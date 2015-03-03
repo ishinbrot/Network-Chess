24ce426b01976491fefd619a7b2f0d85dbf32658
Mike <manhalt1990@gmail.com>
2015-03-03 11:55:51 -0500

    Simple Hello World to get started

---
+++ chessticles/Tests/Implementation/MoveValidationTest.java
@@ -0,0 +1,41 @@
+package Implementation;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MoveValidationTest extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testHorizontal() throws Exception {
+
+    }
+
+    @Test
+    public void testVertical() throws Exception {
+
+    }
+
+    @Test
+    public void testDiagonal() throws Exception {
+
+    }
+
+    @Test
+    public void testL_shape() throws Exception {
+
+    }
+}
