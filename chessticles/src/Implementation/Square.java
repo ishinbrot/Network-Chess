b6c56e2875eb84bbc99e4b80a1de1d8280180f23
Ian Shinbrot <ijs24@drexel.edu>
2015-03-03 14:52:32 -0500

    code for uprading a pawn is in the square class. One line might need to change, otherwise this should work

--- chessticles/src/Implementation/Square.java
+++ chessticles/src/Implementation/Square.java
@@ -41,4 +41,43 @@ public class Square extends JPanel{
     public void setCoord(int[] coord) {
         this.coord = coord;
     }
+    
+    public ChessPiece upgradePawn(ChessPiece pawn)
+    {
+        String input="";
+        do {
+            if ((this.getCoord()[0]==0 && pawn.getColor()==Color.black) || this.getCoord()[0]==8 && pawn.getColor()==Color.white) {
+                String[] choices = {"Queen", "Bishop", "Rook", "Knight"};
+                 input = (String) JOptionPane.showInputDialog(null, "Choose now...",
+                        "The Choice of a Lifetime", JOptionPane.QUESTION_MESSAGE, null, // Use
+                        // default
+                        // icon
+                        choices, // Array of choices
+                        choices[1]); // Initial choice
+            }
+        } while (input=="");
+        
+        if (input=="Queen")
+        {
+            return new Queen(pawn.getColor());
+        }
+        if (input=="Rook")
+        {
+            return new Rook(pawn.getColor());
+        }
+        if (input=="Knight") {
+            return new Knight(pawn.getColor());
+        }
+        else // this should never happen so I'll close the program
+        {
+            this.Error_Message("Something is wrong with the program, aborting...");
+            System.exit(2);
+        }
+        return null;
+    }
+
+    public void Error_Message(String message) {
+        JOptionPane.showMessageDialog(new JFrame(), message, "Dialog",
+                JOptionPane.ERROR_MESSAGE);
+    }
 }
