15390384d3fc4e1709e445f971f14ce6aa9694d8
Ian Shinbrot <ijs24@drexel.edu>
2015-03-10 20:48:35 -0400

    added javadoc

--- chessticles/src/Implementation/Queen.java
+++ chessticles/src/Implementation/Queen.java
@@ -22,6 +22,15 @@ public class Queen extends ChessPiece {
         setValue(10);
         setMoveLimit(8);
     }
+
+    /**
+     * 
+     * @param initialPosition the initial position of the square array
+     * @param finalPosition the final position in the square array
+     * @param board the chessboard
+     * @param lookForCheck if the user would like to look for check
+     * @return
+     */
     public boolean validMove(int[] initialPosition, int[] finalPosition, Square[] board, boolean lookForCheck){
         if (horizontal(initialPosition, finalPosition, board)||
                 vertical(initialPosition, finalPosition, board)||
